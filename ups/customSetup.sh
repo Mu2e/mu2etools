5764eab828b503001a2e9c2457c60f02dc273704
Andrei Gaponenko <gandr@fnal.gov>
2016-05-31 13:41:51 -0500

    Allow to setup mu2ebintools in advance; it won't be overridden.

--- ups/customSetup.sh
+++ ups/customSetup.sh
@@ -4,6 +4,11 @@
 #
 # Andrei Gaponenko, 2016
 
+if [[ -n "${MU2EBINTOOLS_VERSION}" ]]; then
+    # mu2ebintools already setup
+    return 0
+fi
+
 if [[ -n "${MU2E_UPS_QUALIFIERS}" ]]; then
     setup -B mu2ebintools -q ${MU2E_UPS_QUALIFIERS}
     return 0;
@@ -12,6 +17,9 @@ else
 Error: MU2E_UPS_QUALIFIERS environment variable is not set.
 It is needed for setting up the mu2ebintools package.
 Source Offline/setup.sh, or export MU2E_UPS_QUALIFIERS by hand prior to doing "setup mu2etools".
+
+Alternatively, you can setup a correct version of mu2ebintools
+before setting up mu2etools.
 EOF
     return 1;
 fi
