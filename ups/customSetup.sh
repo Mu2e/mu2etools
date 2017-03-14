8985e49c9bc032e919109f44f555e994cb44817c
Andrei Gaponenko <gandr@fnal.gov>
2017-03-14 11:37:27 -0500

    Add config for FHICLCPP v4_03_02

--- ups/customSetup.sh
+++ ups/customSetup.sh
@@ -43,6 +43,7 @@ case "${FHICLCPP_VERSION}" in
     v3_12_06) VER=v1_01_02;;
     v3_12_09) VER=v1_01_03;;
     v4_01_00) VER=v1_01_04;;
+    v4_03_02) VER=v1_01_05;;
 
     *)
         # leave it empty - may be "current" is the correct answer
