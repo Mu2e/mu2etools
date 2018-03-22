6e895ab7ca63047fd102b697266317652234a5ae
Andrei Gaponenko <gandr@fnal.gov>
2018-03-22 10:57:40 -0500

    add fhiclcpp v4_06_06

--- ups/customSetup.sh
+++ ups/customSetup.sh
@@ -44,6 +44,7 @@ case "${FHICLCPP_VERSION}" in
     v4_05_01) VER=v1_01_07;;
     v4_06_03) VER=v1_01_09;;
     v4_06_05) VER=v1_01_10;;
+    v4_06_06) VER=v1_01_12;;
 
     *)
         echo "Error:  unknown FHICLCPP_VERSION ${FHICLCPP_VERSION}. Can not determine the matching version of mu2ebintools." >&2
